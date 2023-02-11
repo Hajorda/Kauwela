f116619abdfc06c0064b9a58f351cecc7a8c7b25
Haj <hajorda@gmail.com>
2023-02-11 19:47:03 +0300

    Config + initial setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
+import javax.security.auth.login.LoginException;
 public class Main {
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    /**
+     * Loads environment variables and builds the bot shard manager.
+     * @throws LoginException occurs when bot token is invalid.
+     */
+    public Main() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("TechnoVisionTV"));
+        shardManager = builder.build();
+    }
+
+    /**
+     * Retrieves the bot environment variables.
+     * @return the DotEnv instance for the bot.
+     */
+    public Dotenv getConfig() { return config; }
+
+    /**
+     * Retrieves the bot shard manager.
+     * @return the ShardManager instance for the bot.
+     */
+    public ShardManager getShardManager() { return shardManager; }
+
+    /**
+     * Main method where we start our bot.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Main bot = new Main();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid!");
+        }
     }
 }
