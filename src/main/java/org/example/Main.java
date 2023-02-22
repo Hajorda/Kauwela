462187de9bd710071e4b5d5827f5fb46e3c3003f
Cicikuss <ahmetbyk49@gmail.com>
2023-02-22 03:06:08 +0300

    ChatGpt Götten veriyo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
-
 import io.github.cdimascio.dotenv.Dotenv;
-import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -11,12 +10,13 @@ import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.example.Commands.CommandManager;
+
 import org.example.Listeners.GuildListener;
-import org.example.Listeners.MessageListener;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import javax.security.auth.login.LoginException;
-import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     private final Dotenv config;
@@ -62,7 +62,8 @@ public class Main {
         timer.start();*/
 
 
-       shardManager.addEventListener(new CommandManager(),new GuildListener(),new MessageListener(config));
+       shardManager.addEventListener(new CommandManager(),new GuildListener());
+
 
 
 
